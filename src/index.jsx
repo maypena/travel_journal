e253554730d64977c5208160814a1a215b31d3df
May Pena <maypena00@gmail.com>
2025-08-26 19:56:42 -0400

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import {createRoot} from "react-dom/client"
+/**
+ * Challenge:
+ * 
+ * - Create an App component in a separate file.
+ *   Import it here and render it
+ * - Create a "components" folder and a Header component.
+ *   Render the Header inside the App component.
+ * - Follow the Travel Journal design to build the Header
+ *   for our page.
+ */
+
+const root = createRoot(document.getElementById("root"))
+root.render(<h1>Hello World</h1>)
