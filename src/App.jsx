b07ba2b403776511c1c2e8314153d84a28184933
May Pena <maypena00@gmail.com>
2025-09-04 20:37:47 -0400

    Use props for data, minor cosmetic changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,22 @@ export default function App() {
     return (
         <>
             <Header />
-            <Entry />
+            <Entry
+                img={{
+                    src: "src/assets/pr.png",
+                    alt: "Beach in Puerto Rico"
+                }}
+                country="Puerto Rico"
+                city="San Juan"
+                mapLink="https://maps.app.goo.gl/igTTtzqrA5j2gu2U8"
+                date="10 Jun 2021 - 12 Jun, 2021"
+                text="Island of Enchantment—in Spanish, Isla del Encanto. 
+                It’s Puerto Rico’s nickname for good reason. 
+                Sandy beaches, palm trees, and tropical breezes make it a 
+                favorite getaway for the sun-and-surf crowd. Rugged mountains 
+                and verdant rainforest attract adventure travelers. 
+                Lavish hotels with oceanside golf courses embrace vacationers who crave luxury."
+            />
         </>
     )
 }
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,19 +1,19 @@
-export default function Entry() {
+export default function Entry(props) {
     return(
         <div className="entry-card">
             <div className="entry-image">
-                <img className="entry-photo" src="src\assets\pr.png" alt="Beach in Puerto Rico" />
+                <img className="entry-photo" src={props.img.src} alt={props.img.alt} />
             </div>
-            <div className="entry-info">
+            <div>
                 <div className="entry-title">
                     <img className="entry-avatar" src="src\assets\yellowpin.png" alt="Yellow pin" />
-                    <span className="entry-country">PUERTO RICO</span>
-                    <a className="entry-link" href="https://maps.app.goo.gl/igTTtzqrA5j2gu2U8">View on Google Maps</a>
+                    <span className="entry-country">{props.country}</span>
+                    <a className="entry-link" href={props.mapLink}>View on Google Maps</a>
                 </div>
-                <div className="entry-details">
-                    <p className="entry-city">San Juan</p>
-                    <p className="entry-date">12 Jan 2023 - 24 Jan, 2023</p>
-                    <p className="entry-content">Island of Enchantment—in Spanish, Isla del Encanto. It’s Puerto Rico’s nickname for good reason. Sandy beaches, palm trees, and tropical breezes make it a favorite getaway for the sun-and-surf crowd. Rugged mountains and verdant rainforest attract adventure travelers. Lavish hotels with oceanside golf courses embrace vacationers who crave luxury.</p>
+                <div>
+                    <p className="entry-city">{props.city}</p>
+                    <p className="entry-date">{props.date}</p>
+                    <p className="entry-content">{props.text}</p>
                 </div>
             </div>
         </div>
