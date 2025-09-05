9030649728a2674d9873fc4fac56768c265996b0
May Pena <maypena00@gmail.com>
2025-09-04 23:43:34 -0400

    use map and data file to create entries

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
+import data from "./data"
 
 export default function App() {
+    const entries = data.map(
+        entry => <Entry
+            img={entry.img}
+            country={entry.country}
+            mapLink={entry.mapLink}
+            city={entry.city}
+            date={entry.date}
+            text={entry.text}
+        />
+    )
     return (
         <>
             <Header />
-            <Entry
-                img={{
-                    src: "src/assets/pr.png",
-                    alt: "Beach in Puerto Rico"
-                }}
-                country="Puerto Rico"
-                city="San Juan"
-                mapLink="https://maps.app.goo.gl/igTTtzqrA5j2gu2U8"
-                date="10 Jun 2021 - 12 Jun, 2021"
-                text="Island of Enchantment—in Spanish, Isla del Encanto. 
-                It’s Puerto Rico’s nickname for good reason. 
-                Sandy beaches, palm trees, and tropical breezes make it a 
-                favorite getaway for the sun-and-surf crowd. Rugged mountains 
-                and verdant rainforest attract adventure travelers. 
-                Lavish hotels with oceanside golf courses embrace vacationers who crave luxury."
-            />
+            {entries}
         </>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,75 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "src/assets/spain.jpg",
+            alt: "Madrid City at night"
+        },
+        country: "Spain",
+        city: "Madrid",
+        mapLink: "https://share.google/EzVWmeClInqok5vko",
+        date: "17 Jan - 12 March, 2020",
+        text: "I studied abroad here for 56 day sbefore the pandemic hit. I had the chance to immerse myself in Spanish culture. Exploring historic landmarks, indulging in tapas, and experiencing the vibrant nightlife made this trip truly special."
+    },
+    {
+        id: 2,
+        img: {
+            src: "src/assets/jamaica.jpg",
+            alt: "Jamaica greenery view from the hotel"
+        },
+        country: "Jamaica",
+        city: "Discovery Bay",
+        mapLink: "https://share.google/vQgOJotWrVRE03Cwa",
+        date: "4 Jun - 9 Jun, 2021",
+        text: "Post-graduation trip with friends! We had an amazing time exploring the beautiful beaches, vibrant culture, and delicious cuisine of Puerto Rico. From relaxing on the sandy shores to dancing to lively music, every moment was unforgettable."
+    },
+    {
+        id: 3,
+        img: {
+            src: "src/assets/pr.png",
+            alt: "Museum of San Juan"
+        },
+        country: "Puerto Rico",
+        city: "San Juan",
+        mapLink: "https://maps.app.goo.gl/igTTtzqrA5j2gu2U8",
+        date: "10 Jun - 12 Jun, 2021",
+        text: "Post-graduation trip with friends! We had an amazing time exploring the beautiful beaches, vibrant culture, and delicious cuisine of Puerto Rico. From relaxing on the sandy shores to dancing to lively music, every moment was unforgettable."
+    },
+    {
+        id: 4,
+        img: {
+            src: "src/assets/portugal.jpg",
+            alt: "Narrow street in Lisbon"
+        },
+        country: "Portugal",
+        city: "Lisbon",
+        mapLink: "https://share.google/PQzB5xGcxcRhgFkDi",
+        date: "29 Apr - 9 May, 2022",
+        text: "I embarked on a friends adventure to Portugal, where I explored the charming streets of Lisbon and indulged in the country's rich history and culture. From tasting traditional pastries to visiting historic sites, this trip was a perfect blend of relaxation and exploration."
+    },
+    {
+        id: 5,
+        img: {
+            src: "src/assets/greece.png",
+            alt: "Beach in Milos"
+        },
+        country: "Greece",
+        city: "Milos/Athens",
+        mapLink: "https://share.google/4wsUmImzVp67JOqS4",
+        date: "11 May - 22 May, 2023",
+        text: "I traveled to Greece with my travel friends to celebrate. We explored the stunning island of Milos, known for its crystal-clear waters and unique rock formations. From relaxing on beautiful beaches to savoring delicious Greek cuisine, this trip was filled with unforgettable experiences and quality time. We also spent a couple of days in Athens, immersing ourselves in the rich history and vibrant culture of the city."
+    },
+    {
+        id: 6,
+        img: {
+            src: "src/assets/colombia.png",
+            alt: "Roofs of Medellin through a cable car window"
+        },
+        country: "Colombia",
+        city: "Medellin",
+        mapLink: "https://share.google/tGmv7qsUoXpL6qnkn",
+        date: "27 May - 2 Jun, 2024",
+        text: "I traveled to Colombia with my travel friends to celebrate. We explored the vibrant city of Medellin, known for its beautiful landscapes and rich culture. From riding cable cars to visiting local markets, this trip was filled with unforgettable experiences and quality time."
+    }
+
+]
